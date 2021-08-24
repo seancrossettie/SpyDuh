032a97d2cff85c74011bf947ce27774132aac6d9
Mitchell-Crumbley <mitchellcrumbley@gmail.com>
2021-08-24 18:24:08 -0500

    Working on Services Repo

---
+++ DataAccess/ServicesRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static SpyDuh.Models.SpyService;
+
+namespace SpyDuh.DataAccess
+{
+    public class ServicesRepository
+    { 
+        static List<ListOfServices> _services = new List<ListOfServices>
+            {
+                new Hat
+                {
+                    Id = Guid.NewGuid(),
+                    Color = "Blue",
+                    Designer = "Charlie",
+                    Style = HatStyle.OpenBack
+                },
+                new Hat
+                {
+                    Id = Guid.NewGuid(),
+                    Color = "Black",
+                    Designer = "Nathan",
+                    Style = HatStyle.WideBrim
+                },
+                new Hat
+                {
+                    Id = Guid.NewGuid(),
+                    Color = "Magenta",
+                    Designer = "Charlie",
+                    Style = HatStyle.Normal
+                }
+            };
+
+        internal Hat GetById(Guid hatId)
+        {
+            return _hats.FirstOrDefault(hat => hat.Id == hatId);
+        }
+
+        internal List<Hat> GetAll()
+        {
+            return _hats;
+        }
+
+        internal IEnumerable<Hat> GetByStyle(HatStyle style)
+        {
+            return _hats.Where(hat => hat.Style == style);
+        }
+
+        internal void Add(Hat newHat)
+        {
+            newHat.Id = Guid.NewGuid();
+
+            _hats.Add(newHat);
+        }
+    }
+}
