d86874b3bb67d443333c40b5de6f25d6b3d39aaf
katherinevfry <katyvfry@gmail.com>
2021-08-23 20:03:25 -0500

    set up spies

---
+++ Controllers/SpiesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SpyDuh.DataAccess;
+using SpyDuh.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpyDuh.Controllers
+{
+    [Route("api/spies")]
+    [ApiController]
+    public class SpiesController : ControllerBase
+    {
+        SpiesRepository _repo;
+        public SpiesController()
+        {
+            _repo = new SpiesRepository();
+        }
+        [HttpGet]
+        public IActionResult GetAllSpies()
+        {
+            return Ok(_repo.GetAll());
+        }
+
+        [HttpPost]
+        public IActionResult AddSpy(Spy newSpy)
+        {
+            if (string.IsNullOrEmpty(newSpy.Name))
+            {
+                return BadRequest("You must provide a name");
+            }
+            _repo.Add(newSpy);
+
+            return Created($"/api/spies/{newSpy.Id}", newSpy);
+        }
+    }
+}
---
+++ DataAccess/SpiesRepository.cs
@@ -0,0 +1,73 @@
+﻿using SpyDuh.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpyDuh.DataAccess
+{
+    public class SpiesRepository
+    {
+        static List<Spy> _spies = new List<Spy>
+       {
+           new Spy
+           {
+              Id =  Guid.NewGuid(),
+              Name = "Inspector Gadget",
+              DoubleAgent = true
+           },
+           new Spy
+           {
+               Id = Guid.NewGuid(),
+               Name = "James Bond",
+               DoubleAgent = false
+           },
+           new Spy
+           {
+               Id = Guid.NewGuid(),
+               Name = "Harriet Tubman",
+               DoubleAgent = false
+           },
+           new Spy
+           {
+               Id = Guid.NewGuid(),  
+               Name = "Sterling Archer",
+               DoubleAgent = true
+           },
+           new Spy
+           {
+               Id = Guid.NewGuid(),
+               Name = "Jason Bourne",
+               DoubleAgent = true
+           },
+           new Spy
+           {
+               Id = Guid.NewGuid(),
+               Name = "Austin Powers",
+               DoubleAgent = true
+           },
+           new Spy
+           {
+               Id = Guid.NewGuid(),
+               Name ="Chuck Bartowski",
+               DoubleAgent = false
+           }
+
+       };
+        internal IEnumerable<Spy> GetAll()
+        {
+            return _spies;
+        }
+
+        internal void Add(Spy newSpy)
+        {
+            newSpy.Id = Guid.NewGuid();
+            _spies.Add(newSpy);
+        }
+
+        internal Spy GetById(Guid spyId)
+        {
+            return _spies.FirstOrDefault(spy => spy.Id == spyId);
+        }
+    }
+}
